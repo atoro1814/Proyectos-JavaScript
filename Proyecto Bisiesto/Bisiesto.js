413d60ea1888f765b1193c9143cbd9fcb90831dc
atoro <atoro@usm.cl>
2025-01-15 12:30:18 -0300

    se realizaron cambios para verificar entre los años 2000 y 3000

--- Proyecto Bisiesto/Bisiesto.js
+++ Proyecto Bisiesto/Bisiesto.js
@@ -1,9 +1,11 @@
-const year = 1700;
 
-if(year % 400 == 0){
-    console.log("El año ", year , " es bisiesto");
-}else if(year % 4 == 0 && year % 100 != 0) {
-    console.log("El año ", year , " es bisiesto");
-}else{
-    console.log("El año ", year , " no es bisiesto");
+
+
+for (let year = 2000; year <= 3000; year++){
+    if(year % 400 == 0){
+        console.log("El año ", year , " es bisiesto");
+    }else if(year % 4 == 0 && year % 100 != 0) {
+        console.log("El año ", year , " es bisiesto");
+    }
 }
+
